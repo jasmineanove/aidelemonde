e7e791515295395fe41b3ca90d006bbb1ad38df8
EC2 Default User <ec2-user@ip-172-31-18-52.us-east-2.compute.internal>
2018-08-03 18:09:47 +0000

    first trial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,26 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
   get '/' do
-    return erb :index
+    return erb :home
   end
+  
+  get '/survey' do
+    return erb :index
+  end  
+  
+  
+  post '/survey' do
+      @irritation_response = params[:irritation]
+      @travel_response = params[:travel]
+      @service_response = params[:service]
+      @donation_response = params[:donation]
+      @info_response = params[:info]
+      
+      surv = Survey.new
+      @message = surv.show(@irritation_response, @travel_response, @service_response, @donation_response, @info_response)
+      
+      return erb :results
+  end
+  
 end
--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -1,3 +1,19 @@
-class SampleModel
-  attr_accessor :name, :age
+class Survey 
+    def show (answer_1, answer_2, answer_3, answer_4, answer_5)
+    
+        return "Your answer for question one was '#{answer_1}'
+        <br>
+         Your answer for question two was '#{answer_2}'
+         <br>
+         Your answer for question three was '#{answer_3}'
+         <br>
+         Your answer for question four was '#{answer_4}'
+         <br>
+         Your answer for question five was '#{answer_5}'"
+         
+
+    end
+
 end
+
+
